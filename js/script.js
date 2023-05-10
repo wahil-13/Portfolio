2045f85baf50bf186d135c0603e2535f43e1732b
wahil-13 <118663448+wahil-13@users.noreply.github.com>
2023-05-10 15:22:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+};
+
+
+
+
+    const typed = new typed('.multiple-text', {
+        strings: ['Etudiant','Développeur web', 'Beat makers'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+        
+    });
+               
